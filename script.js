40de1cf3f169e751d65d19f0f9ef910fce849cc5
Sahil98990 <2022uch0066@iitjammu.ac.in>
2024-07-04 22:50:06 +0530

    Updated

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 console.log("Let's Start !!")
 
+async function getSongs() {
+    let div = document.getElementById("divi");
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+    for(let i=0; i<as.length; i++) {
+        const element = as[i];
+        if(element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    return songs;
+}
+
 async function main() {
-    let a = await fetch("http://127.0.0.1:3000/songs")
-    let resonse = await a.text();
-    console.log(resonse) 
+    let songs = await getSongs();
+    console.log(songs);
+
+    // let songUL = document.querySelector(".songList").getElementsByTagName("tr")[0];
+    // for (const song of songs) {
+    //     songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
+    // }
+
+    
+    // var audio = new Audio(songs[3]);
+    // audio.play();
 }
 
 main();
-
